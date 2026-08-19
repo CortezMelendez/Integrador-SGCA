d377d414ccef49dfba608381a9b6304d19846b3a
CortezMelendez <20253rd043@utez.edu.mx>
2026-08-18 20:57:50 -0600

    Se arreglo el problema con el boton de registrar cotizacion

--- src/main/java/com/sgca/integradorsgca/utils/GenerarHash.java
+++
@@ -1,7 +0,0 @@
-package com.sgca.integradorsgca.utils;
-public class GenerarHash {
-    public static void main(String[] args) {
-        String hash = PasswordUtils.hashPassword("Admin123!");
-        System.out.println(hash);
-    }
-}
